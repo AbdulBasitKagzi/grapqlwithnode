247e026570eb3332e7d151ffa834a5ec95c45119
abdulbasit <imabk@gmail.com>
2022-08-02 18:06:35 +0530

    add a query resolver

--- GQlNode/products/products.resolver.js
+++ GQlNode/products/products.resolver.js
@@ -1,8 +1,13 @@
-const { getProducts } = require("./product.model");
+const { getProducts, getproductsByPrice } = require("./product.model");
 module.exports = {
   Query: {
     products: () => {
       return getProducts();
+    },
+    // when we are not using an earlier argument we can replace it with "_"
+    // here we have replace argument called parent by "_"
+    productsByPrice: (_, args) => {
+      return getproductsByPrice(args.min, args.max);
     },
   },
 };
